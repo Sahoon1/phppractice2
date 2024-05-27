b125fe0b645206c14ccc387cc82e2a8e195a1a5d
Sahoon Hussain Ansari <162339117+Sahoon1@users.noreply.github.com>
2024-05-27 20:23:51 +0500

    work

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+// $admin = "sahoon";
+// $pass = 12345;
+// $admin_02 = "haris";
+// $pass_02 = 12346;
+
+// switch ($admin == "sahoon" && $pass == 12345 || $admin_02 == "haris" && $pass_02 == 123456) {
+//     case true:
+//         echo("Welcome");
+//         break;
+
+//     default:
+//     echo("Login Denied");
+//         break;
+// }
+
+// for ($i = 0; $i < 11; $i++) {
+//     if ($i == 5) {
+//         continue;
+//     }
+//     echo ("<h1>This is For Loop</h1>".$i ." <br>");
+// }
+
+// for ($i = 1; $i <= 5; $i++) {
+//     for ($j = 1; $j <= 3; $j++) {
+//         echo ("this is I" . $i . " this is J " . $j . "<br>");
+//     }
+
+// }
+
+// $a = 0;
+// while ($a <= 10) {
+//     echo ("<h1>This is While Loop</h1>". $a. "<br>");
+//     $a++;
+// }
+?>
